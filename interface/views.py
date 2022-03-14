69360c80d1034c2b8df3bf7e04c6ba44278cd544
YukaLangbuana <langbuana.yuka@hotmail.com>
2022-03-15 01:00:52 +0700

    connected to change

--- interface/views.py
+++ interface/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+import os
 
 def home_view(request):
-    return render(request, "index.html")
+    context = {
+        "kafka_host": os.environ['KAFKA_HOST'],
+        "kafka_port": os.environ['KAFKA_PORT'],
+    }
+    return render(request, "index.html", context)
