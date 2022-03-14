d3805059d5b460cb7ef2353b6a018620edf2a1c2
YukaLangbuana <langbuana.yuka@hotmail.com>
2022-03-14 21:09:14 +0700

    docker

--- kadmin/settings.py
+++ kadmin/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+9j9qvx*w*efqn!la41fz#+*^nw80f+0zf6ycan*w@c1h$@mqf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ['ALLOWED_HOSTS']]
 
 
 # Application definition
